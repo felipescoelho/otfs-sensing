cc9afdea8ad572b1f5d7a5fe81f4c7d59ebb7bc1
felipescoelho <luizfscoelho.92@gmail.com>
2024-09-12 23:52:50 -0300

    update code

--- otfs_isac.py
+++ otfs_isac.py
@@ -70,58 +70,84 @@ def worker(setup: tuple):
     rng = np.random.default_rng(seed=seed)
 
     # Transmitter:
-    x_zc = zadoff_chu(M)
-    x_chirp = chirp(1/Df, 1/(Df*M), M*Df, 1, M)
+    x_zc0 = zadoff_chu(M)
+    x_zc1 = zadoff_chu(int(M/2))
+    x_chirp0 = chirp(1/Df, 1/(Df*M), M*Df, 1, M)
+    x_chirp1 = chirp(1/Df, 1/(Df*M), M*Df, .5, M)
     bc_len = 13
     x_bc = barker_code(bc_len)
-    X_zc = np.zeros((M, N), dtype=np.complex128)
-    X_chirp = np.zeros((M, N), dtype=np.complex128)
+    X_zc0 = np.zeros((M, N), dtype=np.complex128)
+    X_zc1 = np.zeros((M, N), dtype=np.complex128)
+    X_chirp0 = np.zeros((M, N), dtype=np.complex128)
+    X_chirp1 = np.zeros((M, N), dtype=np.complex128)
     X_bc  = np.zeros((M, N), dtype=np.complex128)
-    X_zc[:, 0] = x_zc
-    X_chirp[:, 0] = x_chirp
+    X_zc0[:, 0] = x_zc0
+    X_zc1[:int(M/2), 0] = x_zc1
+    X_chirp0[:, 0] = x_chirp0
+    X_chirp1[:, 0] = x_chirp1
     X_bc[:bc_len, 0] = x_bc
-    X_zc_dt = X_zc @ idft_mat(N)
-    X_chirp_dt = X_chirp @ idft_mat(N)
+    X_zc_dt0 = X_zc0 @ idft_mat(N)
+    X_zc_dt1 = X_zc1 @ idft_mat(N)
+    X_chirp_dt0 = X_chirp0 @ idft_mat(N)
+    X_chirp_dt1 = X_chirp1 @ idft_mat(N)
     X_bc_dt = X_bc @ idft_mat(N)
-    x_zc_t = X_zc_dt.T.flatten()
-    x_chirp_t = X_chirp_dt.T.flatten()
+    x_zc_t0 = X_zc_dt0.T.flatten()
+    x_zc_t1 = X_zc_dt1.T.flatten()
+    x_chirp_t0 = X_chirp_dt0.T.flatten()
+    x_chirp_t1 = X_chirp_dt1.T.flatten()
     x_bc_t = X_bc_dt.T.flatten()
-    X_zc_corr = mf_mat(X_zc[:, 0])
-    X_chirp_corr = mf_mat(X_chirp[:, 0])
+    X_zc_corr0 = mf_mat(X_zc0[:, 0])
+    X_zc_corr1 = mf_mat(X_zc1[:, 0])
+    X_chirp_corr0 = mf_mat(X_chirp0[:, 0])
+    X_chirp_corr1 = mf_mat(X_chirp1[:, 0])
     X_bc_corr = mf_mat(X_bc[:, 0])
     # Memory allocation
-    pd = np.zeros((ensemble, 3))
-    pf = np.zeros((ensemble, 3))
+    pd = np.zeros((ensemble, 5))
+    pf = np.zeros((ensemble, 5))
     for it in range(ensemble):
         # Channel:
         spamseed = rng.integers(99999999)
         targets, targets_pos = gen_targets(seed=spamseed, M=M, N=N, P=P,
                                            Df=Df*1e-3, fc=fc*1e-9)
-        r_zc_t = tx2rx(x_zc_t, targets, M, N, T, fc, snr, spamseed)
-        r_chirp_t = tx2rx(x_chirp_t, targets, M, N, T, fc, snr, spamseed)
+        r_zc_t0 = tx2rx(x_zc_t0, targets, M, N, T, fc, snr, spamseed)
+        r_zc_t1 = tx2rx(x_zc_t1, targets, M, N, T, fc, snr, spamseed)
+        r_chirp_t0 = tx2rx(x_chirp_t0, targets, M, N, T, fc, snr, spamseed)
+        r_chirp_t1 = tx2rx(x_chirp_t1, targets, M, N, T, fc, snr, spamseed)
         r_bc_t = tx2rx(x_bc_t, targets, M, N, T, fc, snr, spamseed)
         # Receiver:
-        Y_zc_dt = r_zc_t.reshape((N, M)).T
-        Y_chirp_dt = r_chirp_t.reshape((N, M)).T
+        Y_zc_dt0 = r_zc_t0.reshape((N, M)).T
+        Y_zc_dt1 = r_zc_t1.reshape((N, M)).T
+        Y_chirp_dt0 = r_chirp_t0.reshape((N, M)).T
+        Y_chirp_dt1 = r_chirp_t1.reshape((N, M)).T
         Y_bc_dt = r_bc_t.reshape((N, M)).T
-        Y_zc_dd = Y_zc_dt @ dft_mat(N)
-        Y_chirp_dd = Y_chirp_dt @ dft_mat(N)
+        Y_zc_dd0 = Y_zc_dt0 @ dft_mat(N)
+        Y_zc_dd1 = Y_zc_dt1 @ dft_mat(N)
+        Y_chirp_dd0 = Y_chirp_dt0 @ dft_mat(N)
+        Y_chirp_dd1 = Y_chirp_dt1 @ dft_mat(N)
         Y_bc_dd = Y_bc_dt @ dft_mat(N)
         # Correlation and normalization:
-        Y_zc = np.abs(X_zc_corr @ Y_zc_dd) @ permute_dft(N) / M
-        Y_chirp = np.abs(X_chirp_corr @ Y_chirp_dd) @ permute_dft(N) / M
+        Y_zc0 = np.abs(X_zc_corr0 @ Y_zc_dd0) @ permute_dft(N) / M
+        Y_zc1 = np.abs(X_zc_corr1 @ Y_zc_dd1) @ permute_dft(N) / M
+        Y_chirp0 = np.abs(X_chirp_corr0 @ Y_chirp_dd0) @ permute_dft(N) / M
+        Y_chirp1 = np.abs(X_chirp_corr1 @ Y_chirp_dd1) @ permute_dft(N) / M
         Y_bc = np.abs(X_bc_corr @ Y_bc_dd) @ permute_dft(N) / bc_len
         # Classify Targets
-        res_zc = classify_targets(find_targets(Y_zc, threshold),
-                                  targets_pos, M, N, P)
-        res_chirp = classify_targets(find_targets(Y_chirp, threshold),
-                                     targets_pos, M, N, P)
+        res_zc0 = classify_targets(find_targets(Y_zc0, threshold),
+                                   targets_pos, M, N, P)
+        res_zc1 = classify_targets(find_targets(Y_zc1, threshold),
+                                   targets_pos, M, N, P)
+        res_chirp0 = classify_targets(find_targets(Y_chirp0, threshold),
+                                      targets_pos, M, N, P)
+        res_chirp1 = classify_targets(find_targets(Y_chirp1, threshold),
+                                      targets_pos, M, N, P)
         res_bc = classify_targets(find_targets(Y_bc, threshold),
                                   targets_pos, M, N, P)
         # Estimate Probabilities
-        pd[it, 0], pf[it, 0] = estimate_pd_pf(res_zc)
-        pd[it, 1], pf[it, 1] = estimate_pd_pf(res_chirp)
-        pd[it, 2], pf[it, 2] = estimate_pd_pf(res_bc)
+        pd[it, 0], pf[it, 0] = estimate_pd_pf(res_zc0)
+        pd[it, 1], pf[it, 1] = estimate_pd_pf(res_zc1)
+        pd[it, 2], pf[it, 2] = estimate_pd_pf(res_chirp0)
+        pd[it, 3], pf[it, 3] = estimate_pd_pf(res_chirp1)
+        pd[it, 4], pf[it, 4] = estimate_pd_pf(res_bc)
     # Average Resutls
     pd_avg = np.mean(pd, axis=0)
     pf_avg = np.mean(pf, axis=0)
@@ -220,9 +246,9 @@ if __name__ == '__main__':
 
             sausage = [int(val) for val in args.snr_lim.split(',')]
             snrs = np.arange(*sausage, 5)
-            thresholds = np.logspace(-5, np.log10(3), 50)
-            pd = np.zeros((3, len(thresholds), len(snrs)))
-            pf = np.zeros((3, len(thresholds), len(snrs)))
+            thresholds = np.logspace(-5, np.log10(4), 100)
+            pd = np.zeros((5, len(thresholds), len(snrs)))
+            pf = np.zeros((5, len(thresholds), len(snrs)))
             if not args.figures:
                 idx_list = list(itertools.product(range(len(snrs)),
                                                   range(len(thresholds))))
@@ -246,7 +272,7 @@ if __name__ == '__main__':
                 # Sort for increasing false alarm rate, drop duplicates, and
                 # interpolate data.
                 pf_axis = np.linspace(0, 1, 1000)
-                pd_interp = np.zeros((3, len(pf_axis), len(snrs)))
+                pd_interp = np.zeros((5, len(pf_axis), len(snrs)))
                 for idx in range(len(snrs)):
                     pd_interp[0, :, idx] = interp1d_curve(pf[0, :, idx],
                                                           pd[0, :, idx],
@@ -256,6 +282,12 @@ if __name__ == '__main__':
                                                           pf_axis)
                     pd_interp[2, :, idx] = interp1d_curve(pf[2, :, idx],
                                                           pd[2, :, idx],
+                                                          pf_axis)
+                    pd_interp[3, :, idx] = interp1d_curve(pf[3, :, idx],
+                                                          pd[3, :, idx],
+                                                          pf_axis)
+                    pd_interp[4, :, idx] = interp1d_curve(pf[4, :, idx],
+                                                          pd[4, :, idx],
                                                           pf_axis)
                 idx_pf = 5
                 idx_snr = 3
@@ -268,9 +300,16 @@ if __name__ == '__main__':
                 # Figure 1 -- ROC
                 fig1 = plt.figure(figsize=(width, height))
                 ax = fig1.add_subplot(111)
-                ax.plot(pf_axis, pd_interp[0, :, idx_snr], label='ZC')
-                ax.plot(pf_axis, pd_interp[1, :, idx_snr], label='Chirp')
-                ax.plot(pf_axis, pd_interp[2, :, idx_snr], label='Barker')
+                ax.plot(pf_axis, pd_interp[0, :, idx_snr], c='tab:blue',
+                        label='Zadoff-Chu-full')
+                ax.plot(pf_axis, pd_interp[1, :, idx_snr], c='tab:green',
+                        label='Zadoff-Chu-half')
+                ax.plot(pf_axis, pd_interp[2, :, idx_snr], '-.', c='tab:orange',
+                        label='Chirp-full')
+                ax.plot(pf_axis, pd_interp[3, :, idx_snr], '-.', c='tab:red',
+                        label='Chirp-half')
+                ax.plot(pf_axis, pd_interp[4, :, idx_snr], c='tab:pink',
+                        label='Barker')
                 ax.set_xlabel('$P_f$')
                 ax.set_ylabel('$P_d$')
                 ax.legend()
@@ -279,12 +318,39 @@ if __name__ == '__main__':
                 # Figure 2 -- Pd vs. SNR
                 fig2 = plt.figure(figsize=(width, height))
                 ax = fig2.add_subplot(111)
-                ax.plot(snrs, pd_interp[0, idx_pf, :], label='ZC')
-                ax.plot(snrs, pd_interp[1, idx_pf, :], label='Chirp')
-                ax.plot(snrs, pd_interp[2, idx_pf, :], label='Barker')
+                ax.plot(snrs, pd_interp[0, idx_pf, :], c='tab:blue',
+                        label='Zadoff-Chu-full')
+                ax.plot(snrs, pd_interp[2, idx_pf, :], '-.', c='tab:orange',
+                        label='Chirp-full')
+                ax.plot(snrs, pd_interp[4, idx_pf, :], c='tab:pink',
+                        label='Barker')
                 ax.set_xlabel('SNR, dB')
                 ax.set_ylabel('$P_d$')
-                ax.legend()
+                ax.legend(ncols=1)
                 fig2.tight_layout()
+
+                # Figure 3 -- Pd vs. SNR half
+                fig3 = plt.figure(figsize=(width, height))
+                ax = fig3.add_subplot(111)
+                ax.plot(snrs, pd_interp[1, idx_pf, :], c='tab:green',
+                        label='Zadoff-Chu-half')
+                ax.plot(snrs, pd_interp[3, idx_pf, :], '-.', c='tab:red',
+                        label='Chirp-half')
+                ax.plot(snrs, pd_interp[4, idx_pf, :], c='tab:pink',
+                        label='Barker')
+                ax.set_xlabel('SNR, dB')
+                ax.set_ylabel('$P_d$')
+                ax.legend(ncols=1)
+                fig3.tight_layout()
+
+                fig1_path = os.path.join(fig_folder,
+                                         f'roc_SNR_{snrs[idx_snr]}.eps')
+                fig2_path = os.path.join(fig_folder,
+                                         f'pd_SNR_Pf_{pf_axis[idx_pf]}.eps')
+                fig3_path = os.path.join(fig_folder,
+                                         f'pd_SNR_Pf_half_{pf_axis[idx_pf]}.eps')
+                fig1.savefig(fig1_path, format='eps', bbox_inches='tight')
+                fig2.savefig(fig2_path, format='eps', bbox_inches='tight')
+                fig3.savefig(fig3_path, format='eps', bbox_inches='tight')
 
                 plt.show()
